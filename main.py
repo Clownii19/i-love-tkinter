bf20dcf72af357532901bc683653028cb96bafa2
Kyle D'Souza <109410141+Clownii19@users.noreply.github.com>
2022-07-16 12:53:31 +0000

    destroyed quiz after questions finished

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class questions:
       
         3: ["Jeff Bezos is the richest person in the world.", 'True', 'False', 2, "False"],
       
-        4: ["‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ The number 1 a prime number‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ", 'True', 'False', 2, "False"],
+        4: ["‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ The number 1 is a prime number‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ", 'True', 'False', 2, "False"],
       
         5: ["‎ ‎ ‎ ‎ ‎ ‎ The Film 'Titanic' has won the most Oscars‎ ‎ ‎ ‎ ‎ ‎ ", 'True', 'False', 1, "True"],
       
@@ -80,15 +80,15 @@ class questions:
 
         
         self.True_button = Button(parent, text = self.questions_answers[qnum][1], font=("Monoid", "35", "bold"), bg="#FFE3D8",height= 2, width= 6, command=lambda: [self.quizprogression(1)])
-        self.True_button.place(x=50,y=310)   #true buttons  
+        self.True_button.place(x=50,y=310)    
 
         self.var1=IntVar()
         
         self.False_button = Button(parent, text = self.questions_answers[qnum][2], font=("Monoid", "35", "bold"), bg="#FFE3D8",height= 2, width= 6, command=lambda: [self.quizprogression(2)])
-        self.False_button.place(x=430,y=310)   #false buttons 
+        self.False_button.place(x=430,y=310)   
 
         self.question_label=Label (parent, text = self.questions_answers[qnum][0],font=("Tw Cen MT", "18", "bold"),bg=background_color, height= 2,)
-        self.question_label.place(x=40, y=40) #header for my questions 
+        self.question_label.place(x=40, y=40) 
 
   def questions_setup(self):
       selector()
@@ -100,9 +100,19 @@ class questions:
   
   def quizprogression(self,x):
     global score 
-    if len(asked)>4: #to determine if its the last question and just end the quiz after 
-      end()
-    else: 
+    if len(asked)>4: #to determine if its the last question and just end the quiz
+      if x==self.questions_answers[qnum][3]: #if choice made is correct
+        score+=1 #add one to score
+        print("right")
+        self.end() #end question section
+      else: #if choice is incorrect
+        score+=0
+        print("wrong")
+        print(choice)
+        messagebox.showinfo("srry","The correct answer was " + self.questions_answers[qnum][4])
+        self.end()
+      
+    else:
       if x==self.questions_answers[qnum][3]: #if choice made is correct
         score+=1
         print("right")#add one to score
@@ -114,14 +124,22 @@ class questions:
         messagebox.showinfo("srry","The correct answer was " + self.questions_answers[qnum][4])
         self.questions_setup() #move to next question
       
-def end(self):
-  
-  
-  
+  def end(self): 
+      self.question_label.destroy()
+      self.True_button.destroy()
+      self.False_button.destroy()
 
 
 
-  if __name__ =="__main__":
+class endscreen: 
+  def __init__(self):
+      self.end_label=Label (parent, text = ("Great Job!" + name), font=("Tw Cen MT", "35", "bold"),bg=background_color, height= 2,)
+      self.end_label.place(x=40, y=40) 
+    
+
+
+
+if __name__ =="__main__":
     root = Tk()
     root.title("general knowledge quiz") 
     root.geometry("700x450")
